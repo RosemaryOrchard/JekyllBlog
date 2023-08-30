0a98d6cebe7c1be14a4c4ce9b0870a1d377f9164
RosemaryOrchard <RosemaryOrchard@users.noreply.github.com>
2023-08-30 01:32:54 +0000

    jekyll build from Action 617ec930285f2bc10dcf574bb9c88ddf3a30f36d

---
+++ js/search.js
@@ -0,0 +1,84 @@
+(function () {
+  var searchTerm = getQueryVariable("query");
+  if (searchTerm) {
+    document.getElementById("search-box").value = searchTerm;
+  }
+
+  function displaySearchResults(results, store) {
+    var searchResults = document.getElementById("search-results");
+
+    if (results.length) {
+      var appendString = "";
+
+      for (var i = 0; i < results.length; i++) {
+        var item = store[results[i].ref];
+        appendString +=
+          '<div><a href="' + item.url + '"><h3>' + item.title + "</h3></a>";
+        appendString += item.excerpt + "...</p></div>";
+      }
+
+      searchResults.innerHTML = appendString;
+    } else {
+      searchResults.innerHTML = "<span>No results found</span>";
+    }
+  }
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, "%20"));
+      }
+    }
+  }
+
+  function searchError() {
+    var searchError = document.getElementById("search-error");
+	var externalSearch = searchTerm + " site:" + window.location.origin;
+    searchError.innerHTML = `Sorry, something went wrong! Try using <a href="https://duckduckgo.com/?q=${encodeURIComponent(externalSearch)}">DuckDuckGo</a> or <a href="https://www.google.com/search?q=${encodeURIComponent(externalSearch)}">Google</a> (this adds <code>site:${window.location.origin}</code> after your search, limiting it to this site!)`;
+  }
+
+  function getPostData() {
+    var url = "search_data.json";
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "json";
+    xhr.onload = function () {
+      var status = xhr.status;
+      if (status === 200) {
+        runSearch(	xhr.response);
+      } else {
+        searchError();
+      }
+    };
+    xhr.send();
+  }
+
+  function runSearch(postData) {
+    let idx = lunr(function () {
+      this.field("id");
+      this.field("title", { boost: 10 });
+      this.field("tags", {boost: 5});
+      this.field("categories");
+      this.field("content", {boost: 5});
+      this.field("excerpt", {boost: 2});
+
+      postData.forEach(function (post) {
+        this.add(post);
+      }, this);
+    });
+
+    if (searchTerm) {
+      document.getElementById("search-box").setAttribute("value", searchTerm);
+
+      var results = idx.search(searchTerm);
+      displaySearchResults(results, window.store);
+    }
+  }
+
+  getPostData();
+})();
